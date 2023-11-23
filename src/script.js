fdc8e71c80d5dbc53bfe342c8d41b94d14b97dc7
Ameer <ameerapex496@gmail.com>
2023-11-23 21:14:06 +0100

    added function to the project

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
+// selecting our elements
+var keyP = document.getElementsByClassName("key-P")[0];
+var numberP = document.getElementsByClassName("number-P")[0];
+var codeP = document.getElementsByClassName("code-P")[0];
 // our eventlisnter on window
 window.addEventListener("keydown", function (event) {
     console.log("Key pressed:", event.key); // Log the pressed key
+    keyP.textContent = event.key;
     if (event.defaultPrevented) {
         console.log("Default action cancelled");
         return; // Should do nothing if the default action has been cancelled
@@ -9,11 +14,13 @@ window.addEventListener("keydown", function (event) {
     if (event.key !== undefined) {
         // Handle the event with KeyboardEvent.key
         console.log("Event handled with KeyboardEvent.key:", event.key);
+        numberP.textContent = event.key;
         handled = true;
     }
     else if (event.keyCode !== undefined) {
         // Handle the event with KeyboardEvent.keyCode
         console.log("Event handled with KeyboardEvent.keyCode:", event.keyCode);
+        codeP.textContent = "".concat(event.keyCode);
         handled = true;
     }
     if (handled) {
@@ -21,4 +28,5 @@ window.addEventListener("keydown", function (event) {
         event.preventDefault();
         console.log("Default action prevented");
     }
-}, true);
+    true;
+});
--- src/script.ts
+++ src/script.ts
@@ -1,30 +1,35 @@
+// selecting our elements
+const keyP = document.getElementsByClassName("key-P")[0] as HTMLAnchorElement;
+const numberP = document.getElementsByClassName(
+  "number-P"
+)[0] as HTMLAnchorElement;
+const codeP = document.getElementsByClassName("code-P")[0] as HTMLAnchorElement;
+
 // our eventlisnter on window
-window.addEventListener(
-  "keydown",
-  (event) => {
-    console.log("Key pressed:", event.key); // Log the pressed key
+window.addEventListener("keydown", function (event) {
+  console.log("Key pressed:", event.key); // Log the pressed key
+  keyP.textContent = event.key;
+  if (event.defaultPrevented) {
+    console.log("Default action cancelled");
+    return; // Should do nothing if the default action has been cancelled
+  }
+  var handled = false;
+  if (event.key !== undefined) {
+    // Handle the event with KeyboardEvent.key
+    console.log("Event handled with KeyboardEvent.key:", event.key);
+    numberP.textContent = event.key;
+    handled = true;
+  } else if (event.keyCode !== undefined) {
+    // Handle the event with KeyboardEvent.keyCode
+    console.log("Event handled with KeyboardEvent.keyCode:", event.keyCode);
+    codeP.textContent = `${event.keyCode}`;
+    handled = true;
+  }
+  if (handled) {
+    // Suppress "double action" if event handled
+    event.preventDefault();
+    console.log("Default action prevented");
+  }
 
-    if (event.defaultPrevented) {
-      console.log("Default action cancelled");
-      return; // Should do nothing if the default action has been cancelled
-    }
-
-    let handled = false;
-    if (event.key !== undefined) {
-      // Handle the event with KeyboardEvent.key
-      console.log("Event handled with KeyboardEvent.key:", event.key);
-      handled = true;
-    } else if (event.keyCode !== undefined) {
-      // Handle the event with KeyboardEvent.keyCode
-      console.log("Event handled with KeyboardEvent.keyCode:", event.keyCode);
-      handled = true;
-    }
-
-    if (handled) {
-      // Suppress "double action" if event handled
-      event.preventDefault();
-      console.log("Default action prevented");
-    }
-  },
-  true
-);
+  true;
+});
