264c092c156d4baa2c4feda3cd4a0f16a5a133bb
Ameer <ameerapex496@gmail.com>
2023-11-23 21:04:36 +0100

    added main function to the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-console.log("hallo world");
+// our eventlisnter on window
+window.addEventListener("keydown", function (event) {
+    console.log("Key pressed:", event.key); // Log the pressed key
+    if (event.defaultPrevented) {
+        console.log("Default action cancelled");
+        return; // Should do nothing if the default action has been cancelled
+    }
+    var handled = false;
+    if (event.key !== undefined) {
+        // Handle the event with KeyboardEvent.key
+        console.log("Event handled with KeyboardEvent.key:", event.key);
+        handled = true;
+    }
+    else if (event.keyCode !== undefined) {
+        // Handle the event with KeyboardEvent.keyCode
+        console.log("Event handled with KeyboardEvent.keyCode:", event.keyCode);
+        handled = true;
+    }
+    if (handled) {
+        // Suppress "double action" if event handled
+        event.preventDefault();
+        console.log("Default action prevented");
+    }
+}, true);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,30 @@
-console.log("hallo world");
+// our eventlisnter on window
+window.addEventListener(
+  "keydown",
+  (event) => {
+    console.log("Key pressed:", event.key); // Log the pressed key
+
+    if (event.defaultPrevented) {
+      console.log("Default action cancelled");
+      return; // Should do nothing if the default action has been cancelled
+    }
+
+    let handled = false;
+    if (event.key !== undefined) {
+      // Handle the event with KeyboardEvent.key
+      console.log("Event handled with KeyboardEvent.key:", event.key);
+      handled = true;
+    } else if (event.keyCode !== undefined) {
+      // Handle the event with KeyboardEvent.keyCode
+      console.log("Event handled with KeyboardEvent.keyCode:", event.keyCode);
+      handled = true;
+    }
+
+    if (handled) {
+      // Suppress "double action" if event handled
+      event.preventDefault();
+      console.log("Default action prevented");
+    }
+  },
+  true
+);
